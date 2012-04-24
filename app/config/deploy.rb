759fa3a5a14db0771559104bfc1afd079230ff25
watanabe <watanabe@tejimaya.com>
2012-04-24 21:44:26 +0900

    add cache dir to shared_files

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_via,  :copy
 set :model_manager, "doctrine"
 # Or: `propel`
 set :shared_files,      ["app/config/parameters.ini", "web/.htaccess"]
-set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :shared_children,     [app_path + "/cache", app_path + "/logs", web_path + "/uploads", "vendor"]
 set :update_vendors, true
 set :deploy_via, :rsync_with_remote_cache
 
