8dade502fd1a15c64dc8aba864325c1f7c86ad21
watanabe <watanabe@tejimaya.com>
2012-04-24 21:02:05 +0900

    modified AppKernel for DefaultBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new PMS\ApiBundle\PMSApiBundle(),
             new PMS\ViewerBundle\PMSViewerBundle(),
+            new Deploy\HtmlBundle\DeployHtmlBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
