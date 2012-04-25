007dca131f336f2118bd0c21135ce45fbfcbdd02
watanabe <watanabe@tejimaya.com>
2012-04-25 15:44:29 +0900

    fixed deploy configuration

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,14 +1,13 @@
-set :application, "pms"
+set :application, "PNEManagerServer"
 set :domain,      "pne.cc"
 set :deploy_to,   "/opt/sabakan/#{domain}"
 set :app_path,    "app"
 
 set :scm,         :git
-set :repository,  "gitolite@localhost:#{application}.git"
+set :repository,  "git@github.com:tejimaya/#{application}.git"
 set :deploy_via,  :copy
 
 set :model_manager, "doctrine"
-# Or: `propel`
 set :shared_files,      ["app/config/parameters.ini", "web/.htaccess", "src/Deploy/HtmlBundle/Resources/config/routing.yml", "src/PMS/ApiBundle/Controller/Listener/RequestListener.php"]
 set :shared_children,     [app_path + "/cache", app_path + "/logs", web_path + "/uploads", "vendor"]
 set :update_vendors, true
