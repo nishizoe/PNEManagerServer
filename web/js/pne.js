516539535e4688dc02080383b3c679a1d09f9ea7
watanabe <watanabe@tejimaya.com>
2012-04-26 14:53:27 +0900

    extract js script

---
+++ web/js/pne.js
@@ -0,0 +1,72 @@
+var domain = "pne.jp";
+var $ = jQuery.noConflict();
+var domainValid = false;
+var mailValid = false;
+var enabled = function() {
+  if (domainValid && mailValid) {
+    $("#sendbutton2").attr("onclick", "send()");
+    $("#sendbutton2").removeAttr("disabled");
+  } else {
+    $("#sendbutton2").removeAttr("onclick");
+    $("#sendbutton2").attr("disabled", "true");
+  }
+};
+$(function(){
+  $("#domain-form").bind("keyup", function(e) {
+    var re = new RegExp(/^[a-z0-9]{3,16}$/);
+    var isValid = re.test($("#domain-form").val());
+    if (isValid) {
+      $.ajax({
+        type: "GET",
+        url: "http://133.242.49.164/api/domain/available",
+        data: "domain=" + $("#domain-form").val() + "." + domain,
+        dataType: "json",
+        success: function(msg) {
+          if (msg.result == true) {
+            $("#available").html("使用可能です");
+            $("#available").css("color", "green");
+            domainValid = true;
+            enabled();
+          } else {
+            $("#available").html("すでに使用されています");
+            $("#available").css("color", "red");
+            domainValid = false;
+            enabled();
+          }
+        }
+      });
+    } else {
+      $("#available").html("3～16字までの半角英数字（小文字）で入力してください。");
+      $("#available").css("color", "red");
+      domainValid = false;
+      enabled();
+    }
+  });
+  $("#mail-form").bind("keyup", function(e){
+    if ($("#mail-form").val() == "") {
+      $("#mailvalid").html("必須項目です。");
+      $("#mailvalid").css("color", "red");
+      mailValid = false;
+      enabled();
+    }else{
+      $("#mailvalid").html("");
+      mailValid = true;
+      enabled();
+    }
+  });
+});
+var send = function(){
+    $.ajax({
+        type: "POST",
+        url: "http://133.242.49.164/api/sns/apply",
+        data: "domain=" + $("#domain-form").val() + '.' + domain + "&email=" + $("#mail-form").val(),
+        dataType: "json",
+        success: function(json){
+          if (json.result == true){
+            document.location = 'https://pne.jp/success.html'
+          }else{
+            alert("エラーです");
+          }
+        },
+    });
+};
