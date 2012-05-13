e902305f941dc9d817bbeefb14d155a03bf67d09
watanabe <watanabe@tejimaya.com>
2012-05-14 03:10:00 +0900

    removed login logic (is it nessesary using API design?)

--- src/PMS/ApiBundle/Controller/AccountController.php
+++ src/PMS/ApiBundle/Controller/AccountController.php
@@ -8,33 +8,8 @@ class AccountController extends BaseApiActionController
     {
         return array(
             'action' => array(
-                'login',
             )
         );
-    }
-
-    public function loginAction()
-    {
-        $request = $this->getRequest();
-        $session = $this->getSession();
-
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
-        {
-             $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        }
-        else
-        {
-             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-        }
-
-        if ((bool)$error) 
-        {
-            return $this->renderJson(array('result' => true));
-        }
-        else
-        {
-            return $this->renderErrorJson(401, array('result' => false));
-        }
     }
 
 }
