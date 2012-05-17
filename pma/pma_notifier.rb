0bbcb746d24c7dc076f46f50ff616311ad5cf0ab
watanabe <watanabe@tejimaya.com>
2012-05-17 13:34:13 +0900

    fixed to use loggger library

--- pma/pma_notifier.rb
+++ pma/pma_notifier.rb
@@ -2,19 +2,22 @@ require 'rubygems'
 
 require 'net/http'
 require 'json'
+require 'logger'
 require 'syslog'
+
+log = Logger.new(STDOUT)
 
 pmshost = 'api.pne.cc'
 pmahost = 'pne.cc'
 
 Syslog.open();
 Syslog.info('start pma_notifier');
-p "start pma_notifer"
+log.info("start pma_notifer")
 if File.exist?("/tmp/.pmalock") then
   Syslog.info('still run ');
-  p "still runn"
+  log.warn("still run")
   Syslog.info('exit pma_notifier');
-  p "exit pma_notifier"
+  log.info("exit pma_notifier")
   exit;
 end
 
@@ -27,4 +30,4 @@ res = http.request_post('/api/server/update', 'host='+pmahost+'&domain='+install
 puts res.body
 
 Syslog.info('end pma_notifier');
-p "end pma_notifier"
+log.info("end pma_notifier")
