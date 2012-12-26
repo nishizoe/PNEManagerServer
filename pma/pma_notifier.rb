5b70917fb00ef2733a03b860f67255145b67e4eb
watanabe <watanabe@tejimaya.com>
2012-12-26 19:46:01 +0900

    update pma scripts

--- pma/pma_notifier.rb
+++ pma/pma_notifier.rb
@@ -1,33 +1,34 @@
 require 'rubygems'
 
-require 'net/http'
+require 'net/https'
 require 'json'
-require 'logger'
 require 'syslog'
 
-log = Logger.new(STDOUT)
-
-pmshost = 'api.pne.cc'
-pmahost = 'pne.cc'
+pmshost = 'pne.jp'
+pmahost = `hostname`.chop
 
 Syslog.open();
 Syslog.info('start pma_notifier');
-log.info("start pma_notifer")
+p "start pma_notifer"
 if File.exist?("/tmp/.pmalock") then
   Syslog.info('still run ');
-  log.warn("still run")
+  p "still runn"
   Syslog.info('exit pma_notifier');
-  log.info("exit pma_notifier")
+  p "exit pma_notifier"
   exit;
 end
 
 installDomains = Dir::entries('/var/www/sns/') - ['.', '..']
 
-http = Net::HTTP.new(pmshost, 80)
+https = Net::HTTP.new(pmshost, 443)
+https.use_ssl = true
+https.ca_file = '/opt/sabakan/hosting/script/pma/ca-bundle.crt'
+https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+https.verify_depth = 5
 
-res = http.request_post('/api/server/update', 'host='+pmahost+'&domain='+installDomains.to_json)
+res = https.request_post('/api/server/update', 'host='+pmahost+'&domain='+installDomains.to_json)
 
 puts res.body
 
 Syslog.info('end pma_notifier');
-log.info("end pma_notifier")
+p "end pma_notifier"
