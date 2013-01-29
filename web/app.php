d993959e79baa709fbed62bf9273f6056303ffd4
suzuki_mar <supasu145@gmail.com>
2013-01-29 14:23:24 +0900

    (refs #17) TOPページにアクセスしたらエラーになるのを修正した

--- web/app.php
+++ web/app.php
@@ -1,12 +1,32 @@
 <?php
 
+//URIが空の場合になぜか正常に実行できないので、index.htmlにリダイレクトさせる
+if ($_SERVER['REQUEST_URI'] === '/')
+{
+  if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+  {
+    $protocl = 'https://';
+  }
+  else
+  {
+    $protocl = 'http://';
+  }
+
+  $redirectURL = $protocl.$_SERVER['SERVER_NAME'].'/index.html';
+  header('Location: '.$redirectURL); exit;
+}
+
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('prod', false);
+//$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+
+//var_dump(Request::createFromGlobalsaa());
+
 //$kernel = new AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();
