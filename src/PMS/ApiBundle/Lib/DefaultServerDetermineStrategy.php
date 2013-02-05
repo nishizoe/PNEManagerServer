cb9ef6caf654fe46f278378a1c83daabf226da54
suzuki_mar <supasu145@gmail.com>
2013-02-05 17:53:34 +0900

    OpenPNEをデプロイするサーバーデータが登録されていなかったら発生する例外メッセージを、何をすればいいのかの説明にかえた
    デプロイじにハマる確率を少なくするため

--- src/PMS/ApiBundle/Lib/DefaultServerDetermineStrategy.php
+++ src/PMS/ApiBundle/Lib/DefaultServerDetermineStrategy.php
@@ -12,7 +12,12 @@ class DefaultServerDetermineStrategy implements ServerDetermineStrategy
         $server = $doctrine->getRepository('PMSApiBundle:Server')->findOneBy(array());
         if (!$server)
         {
-            throw new \RuntimeException('there are no server');
+            $message = "OpnePNEのデプロイで使用するサーバーデータが登録されていません\n";
+            $message .= "以下のコマンドを実行して、サーバーデータを登録してください\n";
+            $message .= "curl \"http://{$_SERVER['SERVER_NAME']}/api/server/add\" -d \"host=[ServerDomain]\"\n";
+            $message .= "[ServerDomain]にはOpenPNEをデプロイするドメイン名を指定してください(pne.jpなど)";
+
+            throw new \RuntimeException($message);
         }
 
         return $server->getId();
